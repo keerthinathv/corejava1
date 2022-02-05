228556aa04584b4b3645093f84fda76a74e9c481
keerthinathv <97837931+keerthinathv@users.noreply.github.com>
2022-02-05 14:51:04 +0530

    Add files via upload

---
+++ AccessSpecifiers3.java
@@ -0,0 +1,16 @@
+//create another package
+package pack2;
+
+public class AccessSpecifiers3 extends Proaccessspecifiers {
+
+	public static void main(String[] args) {
+		AccessSpecifiers3 obj = new AccessSpecifiers3 ();   
+	       obj.display();  
+	}
+
+	private void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
