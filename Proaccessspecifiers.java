cedabfbfa45f6b8f072c36c5e1f6ce7623eb4ae3
keerthinathv <97837931+keerthinathv@users.noreply.github.com>
2022-02-07 11:44:29 +0530

    Add files via upload

--- Proaccessspecifiers.java
+++ Proaccessspecifiers.java
@@ -1,5 +1,9 @@
-package pack2;
+package pack1;
 
 public class Proaccessspecifiers {
 
+	protected void display() 
+    { 
+        System.out.println("This is protected access specifier"); 
+    } 
 }
