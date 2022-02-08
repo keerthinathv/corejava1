9c29e14e224eb19863298deb48da9b89b9b90398
keerthinathv <97837931+keerthinathv@users.noreply.github.com>
2022-02-08 10:15:21 +0530

    Add files via upload

---
+++ InnerClassAssisted3.java
@@ -0,0 +1,16 @@
+package innerClass;
+	
+
+	public class InnerClassAssisted3 {
+
+	public static void main(String[] args) {
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
+
